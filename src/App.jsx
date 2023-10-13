61e91798d466fdcc9f2b1fe67970c3c5a2c1aed7
yurim222 <yurim@ajou.ac.kr>
2023-10-13 13:16:36 +0900

    Feat: header, bottom layout 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import Home from "../src/pages/Home";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
+import MainLayout from "./Components/MainLayout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="w-full">
-      <Routes >
-        <Route path="/" element={<Home />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/Signup" element={<SignupPage />} />
-        <Route path="/main" element={<MainPage />} />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Signup" element={<SignupPage />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/Components/MainLayout/index.jsx
+++ src/Components/MainLayout/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
+import Bottom from "../Bottom";
 function index() {
   return (
     <div className="px-20">
       <Header />
       <Outlet />
+      <Bottom />
     </div>
   );
 }
