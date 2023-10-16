128f8100951ba48abb4d6d591d0e2efd1d2375e9
yurim222 <yurim@ajou.ac.kr>
2023-10-17 01:38:01 +0900

    Feat: 글쓰기 페이지 카테고리 버튼 개발

---
+++ src/pages/WriteBoardPage/Component/ToggleBtn/index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import downBtn from "./img/ic.svg";
+import { useState } from "react";
+function ToggleBtn(props) {
+  const [showMenu, setShowMenu] = useState(false);
+  const onClickHandler = (e) => {
+    setShowMenu(!showMenu);
+    console.log(`${showMenu}`);
+  };
+  return (
+    <div className="">
+      <div
+        className="flex gap-3 border border-1 max-w-fit border-main rounded-lg hover:cursor-pointer"
+        onClick={onClickHandler}
+      >
+        <div className=" text-main text-xs md:text-base font-semibold ml-4 my-2">
+          {props.name}
+        </div>
+        <img src={downBtn} className="mr-4" />
+      </div>
+
+      {/*toggle menu*/}
+      <div
+        className={`${
+          showMenu ? "block" : "hidden"
+        } z-50 fixed top-32 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow text-center`}
+      >
+        <div className="px-4 py-3 ">
+          <ul className="py-4">
+            <li className="border-b-2 ">
+              <button
+                onClick={() => {
+                  setShowMenu(false);
+                }}
+              >
+                이동 봉사
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  setShowMenu(false);
+                }}
+              >
+                대필
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  setShowMenu(false);
+                }}
+              >
+                물품 요청
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  setShowMenu(false);
+                }}
+              >
+                기타
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ToggleBtn;
--- src/pages/WriteBoardPage/index.jsx
+++ src/pages/WriteBoardPage/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import ToggleBtn from "./Component/ToggleBtn";
 
 function WriteBoardPage() {
-  return <div>WriteBoardPage</div>;
+  return (
+    <div className="w-full">
+      <div className=" md:px-56">
+        <div className="text-main font-semibold text-xl my-4">카테고리</div>
+        <div className="mb-10">
+          <ToggleBtn name={"카테고리 선택"} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WriteBoardPage;
