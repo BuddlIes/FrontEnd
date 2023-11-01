7a4df89b168d6bb4b479d2d3fee6d4429a0a3ee5
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2023-11-01 20:20:36 +0900

    Feat:로그인에서 메인 연결

--- src/pages/Login/Components/LogBtn/index.jsx
+++ src/pages/Login/Components/LogBtn/index.jsx
@@ -1,18 +1,18 @@
 //Login
 import React from "react";
 import { Link } from "react-router-dom";
-import BtnLogin from "../../../../../src/assets/button.svg";
-import BtnSign from "../../../../../src/assets/BtnSign.svg";
 
 function Login() {
   return (
     <div>
-      <div className="w-80 h-12 my-8 bg-main flex-1 py-3 px-4 text-base font-semibold text-white text-center hover:bg-[#D6DBDE] hover:text-[#8A8F94] transition-all rounded-lg">
-        로그인
-      </div>
+      <Link to="/main">
+        <div className="w-80 h-12 my-8 bg-main flex-1 py-3 px-4 text-base font-semibold text-white text-center hover:bg-[#D6DBDE] hover:text-[#8A8F94] transition-all rounded-lg">
+          로그인
+        </div>
+      </Link>
       <div className="text-sm text-center text-[#36383B] font-normal">
         버들의 계정이 없으신가요?
-        <Link to="../../../../Signup" className="text-[#0080DD] font-semibold">
+        <Link to="/Signup" className="text-[#0080DD] font-semibold">
           회원가입
         </Link>
       </div>
