94012802de4acb038856b712fdecba8f4b17bc45
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2023-11-11 01:41:24 +0900

    Fix: modal 추가 및 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import Modal from 'react-modal';
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
