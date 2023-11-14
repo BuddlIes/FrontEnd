09fc1e3b79026cc65e23580db050592da0d651d4
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2023-11-14 15:36:01 +0900

    Feat:MyPage 회의 필요

--- src/pages/MyPage/Components/LeftSide/index.jsx
+++ src/pages/MyPage/Components/LeftSide/index.jsx
@@ -3,20 +3,20 @@ import React from "react";
 
 function LeftSide() {
     return (
-        <div className="w-72 h-full ml-60 mr-8 mt-40">
-            <div className="text-4xl font-semibold mb-12">
+        <div className="w-72 h-full ml-12 mr-8 mt-20">
+            <div className="text-4xl font-semibold mb-12 text-[#161718]">
                 My Page
             </div>
-            <div className="w-72 h-12 bg-[#9fc70033] rounded-t-lg p-3 text-base font-semibold">
+            <div className="w-72 h-12 bg-[#9fc70033] rounded-t-lg p-3 text-base font-semibold text-[#161718]">
                 Home
             </div>
-            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold">
+            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold text-[#161718]">
                 내 봉사내역
             </div>
-            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold">
+            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold text-[#161718]">
                 받은 후기 확인
             </div>
-            <div className="w-72 h-12 border border-[#EFEFE4] rounded-b-lg p-3 text-base font-semibold">
+            <div className="w-72 h-12 border border-[#EFEFE4] rounded-b-lg p-3 text-base font-semibold text-[#161718]">
                 관심 봉사
             </div>
         </div>
--- src/pages/MyPage/Components/RightSide/index.jsx
+++ src/pages/MyPage/Components/RightSide/index.jsx
@@ -3,14 +3,21 @@ import React from "react";
 
 function RightSide() {
   return (
-    <div className="w-full h-full mt-40">
-      <div className="text-4xl font-semibold">내 프로필</div>
-      <div className="w-full h-32 py-6 px-5 text-2xl items-center">닉네임</div>
-      <div className="flex flex-col">
-        <div className="w-1/3 h-36 border border-[#D6DBDE] rounded-lg py-6 px-5">
-          <div className="text-lg font-semibold">나의 버들 시간</div>
-          <div className="text-lg font-semibold justify-center text-center items-center"><span className="text-[#749C03] text-5xl">N</span> 시간</div>
+    <div className="w-full h-full mt-20">
+      <div className="text-4xl font-semibold text-[#161718]">내 프로필</div>
+      <div className="w-full h-32 py-6 px-5 text-2xl items-center text-[#161718]">닉네임</div>
+      <div className="flex flex-row mr-8 gap-3">
+        <div className="w-1/2 h-36 border border-[#D6DBDE] rounded-lg py-6 px-5">
+          <div className="text-lg font-semibold mb-2 text-[#161718]">나의 버들 시간</div>
+          <div className="text-lg font-semibold justify-center text-center items-center text-[#161718]"><span className="text-[#749C03] text-5xl">N</span> 시간</div>
         </div>
+        <div className="w-1/2 h-36 border border-[#D6DBDE] rounded-lg py-6 px-5">
+          <div className="text-lg font-semibold mb-2 text-[#161718]">나의 스탬프 개수</div>
+          <div className="text-lg font-semibold justify-center text-center items-center text-[#161718]"><span className="text-[#749C03] text-5xl">N</span> 개</div>
+        </div>
+      </div>
+      <div className="h-12 mt-8 mr-8 bg-[#EFEFE4] text-2xl font-bold px-5 py-1.5 text-[#36383B]">
+        나의 스탬프는 현재 <span className="text-[#749C03]">N</span>개 입니다.
       </div>
     </div>
   );
