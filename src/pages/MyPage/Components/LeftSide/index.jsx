d3b941b4773d6ae7801ec1781ecc1d87710abf3b
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2023-11-14 11:43:21 +0900

    Feat:마이페이지 시작합니다..

--- src/pages/MyPage/Components/LeftSide/index.jsx
+++ src/pages/MyPage/Components/LeftSide/index.jsx
@@ -1,1 +1,14 @@
 //LeftSide
+import React from "react";
+
+function LeftSide() {
+    return (
+        <div>
+            <div>
+                왼쪽
+            </div>
+        </div>
+    );
+}
+
+export default LeftSide;
--- src/pages/MyPage/Components/RightSide/index.jsx
+++ src/pages/MyPage/Components/RightSide/index.jsx
@@ -1,1 +1,14 @@
 //RightSide
+import React from "react";
+
+function RightSide() {
+    return (
+        <div>
+            <div>
+                오른쪽
+            </div>
+        </div>
+    );
+}
+
+export default RightSide;
