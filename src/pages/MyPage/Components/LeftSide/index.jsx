c601ac1772401446741654cb59d6781b8f3679b3
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2023-11-14 14:31:39 +0900

    Feat: LeftSide 완성 및 RightSide 제작 중

--- src/pages/MyPage/Components/LeftSide/index.jsx
+++ src/pages/MyPage/Components/LeftSide/index.jsx
@@ -3,9 +3,21 @@ import React from "react";
 
 function LeftSide() {
     return (
-        <div>
-            <div>
-                왼쪽
+        <div className="w-72 h-full ml-60 mr-8 mt-40">
+            <div className="text-4xl font-semibold mb-12">
+                My Page
+            </div>
+            <div className="w-72 h-12 bg-[#9fc70033] rounded-t-lg p-3 text-base font-semibold">
+                Home
+            </div>
+            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold">
+                내 봉사내역
+            </div>
+            <div className="w-72 h-12 border border-[#EFEFE4] p-3 text-base font-semibold">
+                받은 후기 확인
+            </div>
+            <div className="w-72 h-12 border border-[#EFEFE4] rounded-b-lg p-3 text-base font-semibold">
+                관심 봉사
             </div>
         </div>
     );
--- src/pages/MyPage/Components/RightSide/index.jsx
+++ src/pages/MyPage/Components/RightSide/index.jsx
@@ -2,13 +2,18 @@
 import React from "react";
 
 function RightSide() {
-    return (
-        <div>
-            <div>
-                오른쪽
-            </div>
+  return (
+    <div className="w-full h-full mt-40">
+      <div className="text-4xl font-semibold">내 프로필</div>
+      <div className="w-full h-32 py-6 px-5 text-2xl items-center">닉네임</div>
+      <div className="flex flex-col">
+        <div className="w-1/3 h-36 border border-[#D6DBDE] rounded-lg py-6 px-5">
+          <div className="text-lg font-semibold">나의 버들 시간</div>
+          <div className="text-lg font-semibold justify-center text-center items-center"><span className="text-[#749C03] text-5xl">N</span> 시간</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default RightSide;
